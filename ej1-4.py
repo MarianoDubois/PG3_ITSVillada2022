42c120055eb559929ffafb2a09db8b4a55141d61
MarianoDubois <71388836+MarianoDubois@users.noreply.github.com>
2022-03-25 08:43:10 -0300

    pusheado el ejercicio 4

---
+++ ej1-4.py
@@ -0,0 +1,11 @@
+lista = [3, 5, 1, 6, 0, 2, 7, 8]
+
+def ordenar(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j + 1] :
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    print(arr)
+
+ordenar(lista)
